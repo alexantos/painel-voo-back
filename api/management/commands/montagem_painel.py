6eac9f9a6facf298b0c84361b23f5805a4313322
alexantos <xandreale09@gmail.com>
2025-12-13 11:14:02 -0300

    Validar dados e simular casos de voos com horarios incoerentes para validação da nova task. #8

--- api/management/commands/montagem_painel.py
+++ api/management/commands/montagem_painel.py
@@ -1,8 +1,9 @@
-import datetime
 from datetime import timedelta
 
 from django.core.management.base import BaseCommand
 from time import sleep
+
+from django.utils import timezone
 
 from api.models import Voo, DetalheHorarioVoo, StatusVoo
 
@@ -27,45 +28,47 @@ class Command(BaseCommand):
         voos = Voo.objects.filter(ativo=True)
         for voo in voos:
             detalhe_horario_voo = DetalheHorarioVoo.objects.filter(voo=voo).first()
-            if not StatusVoo.objects.filter(voo=voo).exists():
+            status_voo = StatusVoo.objects.filter(voo=voo)
+            if not status_voo.exists():
                 StatusVoo.objects.create(
                     voo=voo,
                     status="CONFIRMADO",
                 )
-            agora = datetime.datetime.now()
-            horario_embarque = detalhe_horario_voo.previsao_decolagem - timedelta(seconds=15)
-            if agora > horario_embarque:
-                StatusVoo.objects.create(
-                    voo=voo,
-                    status="EMBARCANDO",
-                )
-            ultima_chamada = detalhe_horario_voo.previsao_decolagem - timedelta(seconds=5)
-            if agora > ultima_chamada:
-                StatusVoo.objects.create(
-                    voo=voo,
-                    status="ULTIMA_CHAMADA",
-                )
+            agora = timezone.localtime(timezone.now())
+            if detalhe_horario_voo.previsao_decolagem is not None:
+                horario_embarque = detalhe_horario_voo.previsao_decolagem - timedelta(seconds=15)
+                if agora > horario_embarque and not status_voo.filter(status="EMBARCANDO").exists():
+                    StatusVoo.objects.create(
+                        voo=voo,
+                        status="EMBARCANDO",
+                    )
+                ultima_chamada = detalhe_horario_voo.previsao_decolagem - timedelta(seconds=5)
+                if ultima_chamada is not None and agora > ultima_chamada and not status_voo.filter(status="ULTIMA_CHAMADA").exists():
+                    StatusVoo.objects.create(
+                        voo=voo,
+                        status="ULTIMA_CHAMADA",
+                    )
             decolado = detalhe_horario_voo.horario_decolagem
-            if decolado is not None:
+            if decolado is not None and not status_voo.filter(status="DECOLADO").exists():
                 StatusVoo.objects.create(
                     voo=voo,
                     status="DECOLADO",
                 )
-            aproximando = detalhe_horario_voo.previsao_pouso - timedelta(seconds=10)
-            if agora > aproximando:
-                StatusVoo.objects.create(
-                    voo=voo,
-                    status="APROXIMANDO",
-                )
-
-            pousado = detalhe_horario_voo.previsao_pouso - timedelta(seconds=5)
-            if agora > pousado:
-                StatusVoo.objects.create(
-                    voo=voo,
-                    status="POUSADO",
-                )
-            desembarcando = detalhe_horario_voo.previsao_pouso
-            if desembarcando is not None:
+            if detalhe_horario_voo.previsao_pouso is not None:
+                aproximando = detalhe_horario_voo.previsao_pouso - timedelta(seconds=10)
+                if aproximando is not None and agora > aproximando and not status_voo.filter(status="APROXIMANDO").exists():
+                    StatusVoo.objects.create(
+                        voo=voo,
+                        status="APROXIMANDO",
+                    )
+                pousado = detalhe_horario_voo.previsao_pouso - timedelta(seconds=5)
+                if pousado is not None and agora > pousado and not status_voo.filter(status="POUSADO").exists():
+                    StatusVoo.objects.create(
+                        voo=voo,
+                        status="POUSADO",
+                    )
+            desembarcando = detalhe_horario_voo.horario_pouso
+            if desembarcando is not None and not status_voo.filter(status="DESEMBARCANDO").exists():
                 StatusVoo.objects.create(
                     voo=voo,
                     status="DESEMBARCANDO",
